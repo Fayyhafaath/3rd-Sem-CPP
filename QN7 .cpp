494ab3f626e1295629182827523ea1f4a07b9549
Fayyhafaath <fayhaf123@gmail.com>
2025-10-17 12:48:49 +0530

    QN7 .cpp

---
+++ QN7 .cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Time {
+private:
+    int hours;
+    int minutes;
+    int seconds;
+
+public:
+    
+    void input() {
+        cout << "Enter hours: ";
+        cin >> hours;
+        cout << "Enter minutes: ";
+        cin >> minutes;
+        cout << "Enter seconds: ";
+        cin >> seconds;
+    }
+
+  
+    void display() {
+        cout << hours << " : " << minutes << " : " << seconds << endl;
+    
+    friend Time addTime(Time t1, Time t2);
+};
+
+
